612cbcf7a109e165b5b7810096394732fcdfdcbe
Dhawal Panchamiya <dapanchamiya10@gmail.com>
2026-06-02 17:45:24 +0530

    Storing data in text

--- numpy_work.py
+++ numpy_work.py
@@ -1,4 +1,4 @@
-#In This FIle i will be doing some work with numpy and pandas to get a better understanding of how they work and how to use them in my projects.
+#In This FIle i will be doing some work with numpy to get a better understanding of how they work and how to use them in my projects.
 
 #First i will import the necessary libraries and then i will create some numpy arrays and pandas dataframes to work with.
 import numpy as np
@@ -105,7 +105,7 @@ print(z_score_normalized_total_marks_percentage) #printing the z score normalize
 #Vectorized operations to calculate the pass or fail status of each student based on the marks obtained by each student in all subjects. We will be using the following functions to calculate the pass or fail status using vectorized operations:
 vectorized_pf_status = np.where(data[:,:5].astype(float) >= 40, 1, 0) #calculating the pass or fail status of each student based on the
 
-marks obtained by each student in all subjects by applying the condition to the first 5 columns of the data array using vectorized operations
+#marks obtained by each student in all subjects by applying the condition to the first 5 columns of the data array using vectorized operations
 final_vectorized_pf_status = np.array([1 if np.all(row == 1) else 0 for row in vectorized_pf_status]) #applying the condition to each row of the vectorized_pf_status array to get the final pass or fail status of each student based on the marks obtained by each student in all subjects using vectorized operations
 print("---------------------------------------------------------------------------------------- ")
 print("Pass or Fail Status of Each Student Based on Marks Obtained in All Subjects Using Vectorized Operations: ")
@@ -152,4 +152,27 @@ print("Subject Hardest for Students: Subject ", hardest_subject_index + 1) #prin
 print("Average Marks in Each Subject: ", average_marks_subjects) #printing the average marks in each subject
 print("Average Total Marks Percentage of All Students: ", average_total_marks_percentage) #printing the average total marks percentage of all students
 
-    
+#add all the insights and analysis in a report format and save it as a text file. We will be using the following functions to save the insights and analysis in a report format:
+report = "Report on Student Performance Analysis\n\n" #initializing a string variable to store the report content
+report += "Average Marks of Each Subject: " + str(average_marks) + "\n" #adding the average marks of each subject to the report
+report += "Average Total Marks Percentage of All Students: " + str(average_total_marks_percentage) + "\n" #adding the average total marks percentage of all students to the report
+report += "Standard Deviation of Marks in Each Subject: " + str(std_dev_marks) + "\n" #adding the standard deviation of marks in each subject to the report
+report += "Standard Deviation of Total Marks Percentage of All Students: " + str(std_dev_total_marks_percentage) + "\n" #adding the standard deviation of total marks percentage of all students to the report
+report += "Variance of Marks in Each Subject: " + str(variance_marks) + "\n" #adding the variance of marks in each subject to the report
+report += "Variance of Total Marks Percentage of All Students: " + str(variance_total_marks_percentage) + "\n" #adding the variance of total marks percentage of all students to the report
+report += "Correlation Matrix Between Marks of Each Subject and Total Marks Percentage of All Students: " + str(correlation_matrix) + "\n" #adding the correlation matrix between marks of each subject and total marks percentage of all students to the report
+report += "Normalized Marks in Each Subject: " + str(normalized_marks) + "\n" #adding the normalized marks in each subject to the report
+report += "Normalized Total Marks Percentage of All Students: " + str(normalized_total_marks_percentage) + "\n" #adding the normalized total marks percentage of all students to the report
+report += "Z Score Normalized Marks in Each Subject: " + str(z_score_normalized_marks) + "\n" #adding the z score normalized marks in each subject to the report
+report += "Z Score Normalized Total Marks Percentage of All Students: " + str(z_score_normalized_total_marks_percentage) + "\n" #adding the z score normalized total marks percentage of all students to the report
+report += "Pass or Fail Status of Each Student Based on Marks Obtained in All Subjects Using Vectorized Operations: " + str(final_vectorized_pf_status) + "\n" #adding the pass or fail status of each student based on the marks obtained by each student in all subjects
+report += "Pass or Fail Status of Each Student Based on Marks Obtained in All Subjects Using Loop Based Operations: " + str(loop_based_pf_status) + "\n" #adding the pass or fail status of each student based on the marks obtained by each student in all subjects using loop based operations to the report
+report += "Students Who Improved Consistently: " + str(improved_students) + "\n" #adding the students who have improved consistently in all subjects and total marks percentage of all students to the report
+report += "Students with Highly Uneven Performance: " + str(uneven_performance_students) + "\n" #adding the students who have a high standard deviation in marks across all subjects and total marks percentage of all students to the report
+report += "Subject Hardest for Students: Subject " + str(hardest_subject_index + 1) + "\n"      #adding the subject which is hardest for students based on the lowest average marks in a subject to the report      
+with open("student_performance_report.txt", "w") as file: #opening a text file in write mode to save the report
+    file.write(report) #writing the report content to the text file
+print("---------------------------------------------------------------------------------------- ")
+print("Report has been saved as student_performance_report.txt") #printing a message to indicate that the report has been saved as a text file
+
+#End of the code file.Thank you
