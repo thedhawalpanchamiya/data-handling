f29530b30efbcf37a273e5aa66ff7aad54ef4eb9
Dhawal Panchamiya <dapanchamiya10@gmail.com>
2026-06-04 12:38:18 +0530

    coorelation

--- pandas_work.py
+++ pandas_work.py
@@ -38,6 +38,8 @@ else:    print("There are no missing values in the dataframe.")
 #We will be doing some analysis on the data to get insights from it. We will be dealing with "average score for all subject"
 avg_score = df[['G1', 'G2', 'G3']].mean(axis=1)
 print(avg_score)
+df['avg_score'] = avg_score
+print(df.head())    
 
 #we have avg score now if a student is at 40 percentile or above he is considered as pass otherwise fail. We will be creating a function named pass_fail and use avg_score as input and generate a new np array contain 0 and 1 where 1 is for pass and 0 is for fail. We will then add this new array as a new column in the dataframe and print the first 5 rows of the dataframe to see the new column.
 def pass_fail(score):
@@ -69,3 +71,13 @@ if df['G1'].mean() < df['G2'].mean() < df['G3'].mean():
     print("There is an improvement in the scores of the students over time.")
 else:
     print("There is no improvement in the scores of the students over time.")
+
+#Now lets have a look at coorelation between gender and score of the students. We will be using the groupby function to group the data by gender and then we will be calculating the average score for each gender and then we will be comparing the average scores of both genders to see if there is any difference in the scores of the students based on their gender.
+
+gender_comparison = df.groupby('sex')['avg_score'].mean()
+print(gender_comparison)
+
+#Now lets see effects of study time on the scores of the students. We will be using the groupby function to group the data by study time and then we will be calculating the average score for each study time and then we will be comparing the average scores of each study time to see if there is any difference in the scores of the students based on their study time.
+study_time_comparison = df.groupby('studytime')['avg_score'].mean()
+print(study_time_comparison)
+
